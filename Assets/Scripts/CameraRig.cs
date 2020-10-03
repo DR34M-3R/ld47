7211b0ee7f4b911b6890a85fa0282b74085fb920
Varbin <nikita.severin2788@gmail.com>
2020-10-03 15:45:45 +0300

    CharacterAndCamera

---
+++ Assets/Scripts/CameraRig.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour
+{
+    public static CameraRig Instance;
+    private Camera CameraComponent;
+
+    [SerializeField] private Transform PlayerTransform;
+    [SerializeField] private float zoomDistance;
+    [SerializeField] private float offsetY;
+    [SerializeField] private float moveSpeed;
+    //[SerializeField] private float Zoffset;
+
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    private void Start() {
+        CameraComponent = GetComponent<Camera>();
+
+        PlayerTransform = CharacterController.Instance.GetComponent<Transform>();
+    }
+
+    private void FixedUpdate() {
+        CameraUpdate();
+    }
+
+    public void SetZoom(float zoom) {
+        zoomDistance = zoom;
+    }
+    public void SetOffsetY(float offset) {
+        offsetY = offset;
+    }
+
+    private void CameraUpdate() {
+        Vector3 targetPoint = new Vector3(PlayerTransform.transform.position.x,PlayerTransform.transform.position.y + offsetY ,-zoomDistance);
+        transform.position = Vector3.Lerp(transform.position,targetPoint,moveSpeed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour{
+
+    public static CharacterController Instance;
+
+    public Rigidbody2D RB;
+    public float Axis;
+    public float moveSpeed;
+
+
+    public void Awake() {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        Axis = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate() {
+        Move();
+    }
+
+    private void Move() {
+
+        RB.velocity = new Vector2(Axis * moveSpeed,RB.velocity.y);
+    }
+}
