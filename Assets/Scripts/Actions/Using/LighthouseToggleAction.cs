2868a4be08c58bd55983c0c40cd4fcf72330b9bf
Nickfried <bobylev.na@gmail.com>
2020-10-05 20:45:10 +0300

    WIP

---
+++ Assets/Scripts/Actions/Using/LighthouseToggleAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LighthouseToggleAction : BaseUsableAction
+{
+    public GameObject opened;
+    private bool isOpen;
+
+    public override void Run()
+    {
+        if (!isOpen)
+        {
+            isOpen = Inventory.Instance.RemoveItem(ItemType.safeCode);
+            if (isOpen)
+            {
+                SubtitlesController.Instance.Show(Loc.Get("safe_opened"));
+                opened.active = true;
+            }
+            else
+            {
+                SubtitlesController.Instance.Show(Loc.Get("safe_closed"));
+            }
+        }
+    }
+}
